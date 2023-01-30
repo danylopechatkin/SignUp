5263e3a3e1a701f40a38345219b97cab6d17ec65
Danylo <pechatkin.work@gmail.com>
2023-01-31 00:06:31 +0100

    Setup ViewScreen

---
+++ SignUp/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  SignUp
+//
+//  Created by Danylo Pechatkin on 28/01/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func loginButtonPress(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonPress(_ sender: Any) {
+    }
+    
+}
---
+++ SignUp/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  SignUp
+//
+//  Created by Danylo Pechatkin on 28/01/2023.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addImageButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    
+}
